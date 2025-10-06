4e73dde19f6881ba2b7e3a1c7317241f4dfa7c78
RamonCollazo <73500190+RamonCollazo@users.noreply.github.com>
2025-10-06 14:23:22 -0400

    added some conditional examples

---
+++ python/1_conditionals/0_if.py
@@ -0,0 +1,12 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# Boolean expression. Yes or no. True or false
+if x < y:
+    print("x is less than y")
+
+if x > y:
+    print("x is greater than y")
+
+if x == y:
+    print("x is equal to y")
